d5961945a074bb32b071420ebfdffd4956e7114c
Soul Bruteflow <Soul-Bruteflow@users.noreply.github.com>
2019-03-31 16:09:52 +0300

    ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bruteflow <bruteflow@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/31 16:06:58 by bruteflow         #+#    #+#             */
+/*   Updated: 2019/03/31 16:09:32 by bruteflow        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	unsigned long	len;
+	char			*dest;
+
+	len = 0;
+	while (src[len])
+		len++;
+	dest = malloc(sizeof(char) * len + 1);
+	len = 0;
+	while (src[len])
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
