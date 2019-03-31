6254e5bc958d5dd8ae1445eb9975dbb8111fb919
Soul Bruteflow <Soul-Bruteflow@users.noreply.github.com>
2019-03-31 14:42:06 +0300

    ex13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bruteflow <bruteflow@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/31 14:41:12 by bruteflow         #+#    #+#             */
+/*   Updated: 2019/03/31 14:41:14 by bruteflow        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_recursive_factorial(int nb)
+{
+	int res;
+
+	res = 1;
+	if (nb == 0 || nb == 1)
+		return (1);
+	if (nb < 0 || nb > 12)
+		return (0);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
